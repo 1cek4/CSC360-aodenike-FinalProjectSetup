797c3cbbbffe55074a1520aeb242dd45ad41a687
1cek4 <67841585+1cek4@users.noreply.github.com>
2025-11-24 19:09:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ sword = weaponFactory.createWeapon(
 print(f"Weapon Name: {sword.name}")
 
 
-
+#Facade added to simplify api usage, users dont need to know to create a factory nor implement the specific enums the facade handles those for them.
 api = apiFacade.apiFacade()
 print(api.createWeapon("acidEdge", "Axe", (30, 5, 7), "II", [("Acidic Crystal", 5), ("Steel Ingot", 8)], (80, "Gold"), "A sword that deals acid damage."))
